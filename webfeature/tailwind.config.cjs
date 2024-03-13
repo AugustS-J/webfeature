93f16e92a79a5e8f7ef85ce8e3bad674b7b20bdf
AugustS-J <ausc0001@stud.kea.dk>
2024-03-13 16:51:34 +0100

    typografi, farvepalette, costum curser og lyde

--- webfeature/tailwind.config.cjs
+++ webfeature/tailwind.config.cjs
@@ -1,5 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Aardvark Fixed'],
+            },
+            cursor: {
+                'hand-cursor': 'url(greencursor.png), pointer',
+            }
+        }
+    },
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     plugins: [
         require('@tailwindcss/typography'),
